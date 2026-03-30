769a04218c6153e2861e0bd8086659d270871b25
GabrielRodriguez <gabrielrodriguezvqz@gmail.com>
2026-03-30 17:59:38 -0500

    Base

---
+++ main.py
@@ -0,0 +1,45 @@
+from services.csv_services import *
+from services.student_services import (
+    add_student,
+    list_students,
+    search_student
+)
+
+def show_menu():
+    print("1. Register new Student")
+    print("2. List")
+    print("3. Search student")
+    print("7. Exit")
+
+
+def main():
+    """Main function — loads data and runs the menu loop."""
+    print("Student System")
+    print("")
+
+    # Load existing clients from CSV on startup
+    students = load_students_csv()
+    print(f"✅ {len(students)} client(s) loaded.\n")
+
+    while True:
+        show_menu()
+        option = input("Select an option (1-7): ").strip()
+
+        if option == "1":
+            add_student(students)
+        elif option == "2":
+            list_students(students)
+        elif option == "3":
+            search_student(students)
+        elif option == "7":
+            print("\n Goodbye! All data has been saved.")
+            break
+        else:
+            print(" Invalid option. Please choose between 1 and 7.")
+
+
+
+
+# Entry point
+if __name__ == "__main__":
+    main()
---
+++ src/models/student.py
@@ -0,0 +1,36 @@
+
+class Student:
+    """Represents a gym client with all their information."""
+
+    def __init__(self, id, name, age, program, status="active"):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.program = program        # monthly / quarterly / annual
+        self.status = status    # active / inactive
+
+    def to_dict(self):
+        """Converts the client object into a dictionary."""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "program": self.program,
+            "status": self.status
+        }
+
+    @staticmethod
+    def from_dict(data):
+        """Creates a Client object from a dictionary."""
+        return Student(
+            id=int(data["id"]),
+            name=data["name"],
+            age=int(data["age"]),
+            program=data["program"],
+            status=data["status"]
+        )
+
+    def __str__(self):
+        """Returns a readable string representation of the client."""
+        return (f"ID: {self.id} | Name: {self.name} | Age: {self.age} | "
+                f"Program: {self.program} | Status: {self.status}")
